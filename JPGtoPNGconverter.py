1cd20f75f4958de02662130cbb153d48e7c93f47
aneagoie <aneagoie1@gmail.com>
2019-09-08 11:24:11 +1200

    pika

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from PIL import Image
+
+path = sys.argv[1]
+directory = sys.argv[2]
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+for filename in os.listdir(path):
+  clean_name = os.path.splitext(filename)[0]
+  img = Image.open(f'{path}{filename}')
+  #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+  img.save(f'{directory}/{clean_name}.png', 'png')
+  print('all done!')
+
